142bd2719eb76d99df22331a107428dc1f20cfd1
Trevor Bruecher <trevor.bruecher@avanade.com>
2019-02-12 10:18:07 -0800

    part4

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import {ProductService } from '../product.service';
 import { Response } from '@angular/http';
+import { Album } from '../album';
 
 @Component({
   selector: 'app-product-description',
@@ -10,8 +11,8 @@ import { Response } from '@angular/http';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albumInfo;
-  
+  albumInfo: Album;
+
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
